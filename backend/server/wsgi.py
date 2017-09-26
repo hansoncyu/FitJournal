261e56fc9ac629fa21f7638743d365da811a0a82
Hanson Yu <hansoncyu@gmail.com>
2017-09-26 00:57:08 -0400

    tried changing app name

--- backend/server/wsgi.py
+++ backend/server/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 import sys
 
-sys.path.append('/opt/python/current/app/server')
+sys.path.append('/opt/python/current/app/backend')
 
 from django.core.wsgi import get_wsgi_application
 
