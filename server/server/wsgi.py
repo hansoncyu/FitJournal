2ebb6d1fda8faa8b05f5f2fad2a457dcc5bd4874
Hanson Yu <hansoncyu@gmail.com>
2017-09-26 00:30:38 -0400

    added print statement

--- server/server/wsgi.py
+++ server/server/wsgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+for path in sys.path:
+    print(path)
 
 from django.core.wsgi import get_wsgi_application
 
